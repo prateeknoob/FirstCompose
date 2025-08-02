84bd29d3d63913fda11b6122690835be19a28daf
Prateek345 <prateeksingh.ps345@gmail.com>
2025-08-02 05:42:34 +0530

    first push

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -1,47 +1,113 @@
 package com.example.firstcompose
 
+
+import android.R.attr.label
+import androidx.compose.ui.text.font.FontStyle
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
 import com.example.firstcompose.ui.theme.FirstComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            FirstComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            enableEdgeToEdge()
+            Text(
+                text = "Hello World",
+                modifier = Modifier.statusBarsPadding()
+            )
+
+            TextInput()
         }
     }
 }
 
+
+
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun Previewfun(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            "Hello Prateek",
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.ExtraBold,
+            color = Color.Red,
+            fontSize = 36.sp,
+            textAlign = TextAlign.Center,
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Dummy Image",
+            colorFilter = ColorFilter.tint(Color.Blue),
+            contentScale = ContentScale.Crop
+        )
+
+        Button(
+            onClick = {}, colors = ButtonDefaults.buttonColors(
+                contentColor = Color.White,
+                containerColor = Color.Black,
+            ),
+            enabled = true
+        ) {
+            Text(text = "Hello")
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "Dummy Image"
+            )
+        }
+        TextField(value = "Prateek Singh", onValueChange = {},
+            label ={ Text(text = "Enter Name")},
+            placeholder = {}
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    FirstComposeTheme {
-        Greeting("Android")
-    }
+fun TextInput(){
+    val state= remember{ mutableStateOf("")}
+    TextField(
+        value = state.value,
+        onValueChange = {
+            state.value=it
+        },
+        label ={ Text(text = "Enter Message")}
+    )
 }
